7987cfcc99a4339d91c194872050b90aa5d3c0d1
SpookyyMoon <spookyluna666@gmail.com>
2025-10-09 10:40:26 +0200

    Imagenes inicio, incio de controlador de juego

--- src/main/java/com/example/apalabradosjavafx/JuegoController.java
+++ src/main/java/com/example/apalabradosjavafx/JuegoController.java
@@ -1,4 +1,44 @@
 package com.example.apalabradosjavafx;
 
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
 public class JuegoController {
+
+    // Labels contenedores de letras
+    // Primera fila
+    @FXML private Label letra1 = new Label();
+    @FXML private Label letra2 = new Label();
+    @FXML private Label letra3 = new Label();
+    @FXML private Label letra4 = new Label();
+    @FXML private Label letra5 = new Label();
+
+    // Segunda fila
+    @FXML private Label letra6 = new Label();
+    @FXML private Label letra7 = new Label();
+    @FXML private Label letra8 = new Label();
+    @FXML private Label letra9 = new Label();
+    @FXML private Label letra10 = new Label();
+
+    // Tercera fila
+    @FXML private Label letra11 = new Label();
+    @FXML private Label letra12 = new Label();
+    @FXML private Label letra13 = new Label();
+    @FXML private Label letra14 = new Label();
+    @FXML private Label letra15 = new Label();
+
+    // Cuarta fila
+    @FXML private Label letra16 = new Label();
+    @FXML private Label letra17 = new Label();
+    @FXML private Label letra18 = new Label();
+    @FXML private Label letra19 = new Label();
+    @FXML private Label letra20 = new Label();
+
+    // Quinta fila
+    @FXML private Label letra21 = new Label();
+    @FXML private Label letra22 = new Label();
+    @FXML private Label letra23 = new Label();
+    @FXML private Label letra24 = new Label();
+    @FXML private Label letra25 = new Label();
 }
